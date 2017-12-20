a919dfb2ee56c6e1a3f8497aaad842dca0b5ac7b
Maxime Maheo <imaxame@gmail.com>
2017-12-20 20:44:21 +0100

    Get bitcoin value

--- app/Services/CoinDeskService.php
+++ app/Services/CoinDeskService.php
@@ -9,7 +9,35 @@
 namespace App\Services;
 
 
+use App\Point;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
 class CoinDeskService
 {
+    public static function getPriceOfDay($day)
+    {
+        $start = Carbon::create($day->year, $day->month, $day->day - 1)->toDateString();
+        $end = $day->toDateString();
 
+        $client = new Client();
+        $request = $client->request('GET', 'https://api.coindesk.com/charts/data?data=close&exchanges=bpi&dev=1&index=USD&startdate=' . $start . '&enddate=' . $end);
+        $response = str_replace("cb(", "", $request->getBody()->getContents());
+        $response = str_replace(");", "", $response);
+        $points = json_decode($response, true)["bpi"];
+
+        foreach ($points as $point) {
+            $timeStamp = (int)substr((string)$point[0], 0, -3);
+            $date = Carbon::createFromTimestamp($timeStamp);
+            $start = new Carbon($start);
+            $end = new Carbon($end);
+
+            if ($date->between($start, $end)) {
+                Point::create([
+                    'date'  => $date,
+                    'value' => $point[1],
+                ]);
+            }
+        }
+    }
 }
