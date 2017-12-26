a1f51f0290edc0370895f5ca74de4b894f529a8a
Maxime Maheo <imaxame@gmail.com>
2017-12-26 14:19:33 +0100

    setup server

---
+++ setupServer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+ssh root@$0
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+git clone git@github.com:mmaheo/Trading.git
+cd Trading/laradock
+cp env-example .env
+docker-compose up -d nginx mysql
+chmod 777 ./setupProject.sh
+docker exec -it laradock_workspace_1 ./setupProject.sh
