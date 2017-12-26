75d6038794833905cdc6572adf11969a0d77f19c
Maxime Maheo <imaxame@gmail.com>
2017-12-26 14:39:38 +0100

    Setup server

--- installTools.sh
+++ installTools.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-sudo systemctl status docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-cd laradock
-cp env-example .env
-docker-compose up -d nginx mysql
-cd ..
-chmod 777 ./setupProject.sh
-docker exec -it laradock_workspace_1 ./setupProject.sh
+ssh root@$1 curl https://raw.githubusercontent.com/mmaheo/Trading/master/setupServer.sh
--- setupProject.sh
+++ setupProject.sh
@@ -3,3 +3,5 @@ cp .env.example .env
 composer install
 php artisan key:generate
 php artisan migrate
+chmod 777 -R storage/
+exit
